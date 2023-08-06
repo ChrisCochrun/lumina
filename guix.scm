50a8b618ada2cad86c75695c30187faa65fee16e
Chris Cochrun <chris@cochrun.xyz>
2023-08-06 13:10:15 -0500

    update to guix.scm

--- guix.scm
+++ guix.scm
@@ -22,6 +22,7 @@
              (gnu packages mold)
              (gnu packages pkg-config)
              (gnu packages kde-frameworks)
+             (gnu packages kde)
              (gnu packages video)
              (gnu packages cmake)
              (gnu packages crates-io)
@@ -69,37 +70,50 @@
       (synopsis "Adding rust to cmake projects")
       (description "idk"))))
 
-(define-public rust-cxx-qt-1
+(define-public qtfull
   (package
-    (name "rust-cxx")
-    (version "1.0.86")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (crate-uri "cxx" version))
-        (file-name
-         (string-append name "-" version ".tar.gz"))
-        (sha256
-         (base32
-          "0yc5gz723hiwqk7waygj63655fh5vzq3551p1j2wyzc06xf0glai"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:tests? #f  ; Cannot compile cxx-test-suite.
-       #:cargo-inputs
-       (("rust-cc" ,rust-cc-1)
-        ("rust-cxxbridge-flags" ,rust-cxxbridge-flags-1)
-        ("rust-cxxbridge-macro" ,rust-cxxbridge-macro-1)
-        ("rust-link-cplusplus" ,rust-link-cplusplus-1))
-       #:cargo-development-inputs
-       (("rust-cxx-build" ,rust-cxx-build-1)
-        ("rust-cxx-gen" ,rust-cxx-gen-0.7)
-        ("rust-cxx-test-suite" ,rust-cxx-test-suite-0.0.0)
-        ("rust-rustversion" ,rust-rustversion-1)
-        ("rust-trybuild" ,rust-trybuild-1))))
-    (home-page "https://cxx.rs")
-    (synopsis "Safe interop between Rust and C++")
-    (description "This package provides a safe interop between Rust and C++.")
-    (license (list license:expat license:asl2.0))))
+    (inherit qtbase-5)
+    (propagated-inputs (modify-inputs
+                        (package-inputs qtbase-5)
+                        (append
+                         qtdeclarative-5
+                         qtquickcontrols2-5
+                         qtx11extras
+                         qtwayland-5
+                         qtwebengine-5
+                         qttools-5)))))
+
+;; (define-public rust-cxx-qt-1
+;;   (package
+;;     (name "rust-cxx")
+;;     (version "1.0.86")
+;;     (source
+;;       (origin
+;;         (method url-fetch)
+;;         (uri (crate-uri "cxx" version))
+;;         (file-name
+;;          (string-append name "-" version ".tar.gz"))
+;;         (sha256
+;;          (base32
+;;           "0yc5gz723hiwqk7waygj63655fh5vzq3551p1j2wyzc06xf0glai"))))
+;;     (build-system cargo-build-system)
+;;     (arguments
+;;      `(#:tests? #f  ; Cannot compile cxx-test-suite.
+;;        #:cargo-inputs
+;;        (("rust-cc" ,rust-cc-1)
+;;         ("rust-cxxbridge-flags" ,rust-cxxbridge-flags-1)
+;;         ("rust-cxxbridge-macro" ,rust-cxxbridge-macro-1)
+;;         ("rust-link-cplusplus" ,rust-link-cplusplus-1))
+;;        #:cargo-development-inputs
+;;        (("rust-cxx-build" ,rust-cxx-build-1)
+;;         ("rust-cxx-gen" ,rust-cxx-gen-0.7)
+;;         ("rust-cxx-test-suite" ,rust-cxx-test-suite-0.0.0)
+;;         ("rust-rustversion" ,rust-rustversion-1)
+;;         ("rust-trybuild" ,rust-trybuild-1))))
+;;     (home-page "https://cxx.rs")
+;;     (synopsis "Safe interop between Rust and C++")
+;;     (description "This package provides a safe interop between Rust and C++.")
+;;     (license (list license:expat license:asl2.0))))
 
 
 (define-public lumina
@@ -124,7 +138,7 @@
                              mold
                              gdb
                              pkg-config
-                             qtbase-5
+                             qtfull
                              qttools-5
                              qt-creator
                              qtdeclarative-5
@@ -133,6 +147,7 @@
                              qtwayland-5
                              qtwebengine-5
                              kirigami
+                             kirigami-addons
                              qqc2-desktop-style
                              extra-cmake-modules
                              karchive
