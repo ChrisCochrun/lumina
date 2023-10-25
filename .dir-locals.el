791ded9c730054c547abfdcc6de60e22ac920be2
Chris Cochrun <chris@cochrun.xyz>
2023-10-25 09:36:00 -0500

    updates to dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -B bld/ . && make -j8 --dir bld/ && rm -rf ~/.cache/librepresenter/LibrePresenter/qmlcache/")))
- (prog-mode . ((compile-command . "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -B bld/ . && make -j8 --dir bld/ && rm -rf ~/.cache/librepresenter/LibrePresenter/qmlcache/"))))
+((nil . ((compile-command . "just build")))
+ (prog-mode . ((compile-command . "just build"))))
