04c8e84eaa9694a69b79f16743eb641fe40cc1cc
Chris Cochrun <chris@cochrun.xyz>
2024-06-24 16:44:05 -0500

    also remove the compile of the file

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ use cxx_qt_build::CxxQtBuilder;
 
 fn main() {
     CxxQtBuilder::new()
-        .file("src/rust/service_thing.rs")
         .file("src/rust/settings.rs")
         .file("src/rust/file_helper.rs")
         .file("src/rust/slide_object.rs")
