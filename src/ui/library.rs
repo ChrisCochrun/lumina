991baa8f327d7d247f8e22d9ffae3cd0b974ab14
Chris Cochrun <chris@cochrun.xyz>
2024-12-17 06:40:53 -0600

    beginning to add library component

---
+++ src/ui/library.rs
@@ -0,0 +1,31 @@
+use cosmic::{Element, Task};
+
+use crate::core::{
+    images::Image, model::Model, presentations::Presentation,
+    songs::Song, videos::Video,
+};
+
+struct Library {
+    song_library: Model<Song>,
+    image_library: Model<Image>,
+    video_library: Model<Video>,
+    presentation_library: Model<Presentation>,
+}
+
+enum Message {
+    AddItem,
+    None,
+}
+
+impl Library {
+    pub fn update(&mut self, message: Message) -> Task<Message> {
+        match message {
+            Message::AddItem => todo!(),
+            Message::None => todo!(),
+        }
+    }
+
+    pub fn view(&self) -> Element<Message> {
+        todo!()
+    }
+}
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,2 +1,3 @@
+pub mod library;
 pub mod presenter;
 pub mod video;
