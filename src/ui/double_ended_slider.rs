62453866c5334901297ba9451a489a550183c8db
Chris Cochrun <chris@cochrun.xyz>
2025-03-08 22:35:35 -0600

    beginning to create a double_ended_slider

---
+++ src/ui/double_ended_slider.rs
@@ -0,0 +1,15 @@
+use std::ops::RangeInclusive;
+
+use cosmic::iced::Length;
+
+struct DoubleSlider<'a, T, Message> {
+    range: RangeInclusive<T>,
+    step: T,
+    beginning_value: T,
+    end_value: T,
+    default_beginning: Option<T>,
+    default_end: Option<T>,
+    on_change: Box<dyn Fn(T) -> Message + 'a>,
+    width: Length,
+    height: i32,
+}
