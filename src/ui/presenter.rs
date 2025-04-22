fc445e929f33510c1b229be5331afeb1f499a033
Chris Cochrun <chris@cochrun.xyz>
2025-04-22 09:46:33 -0500

    remove stroke

--- src/ui/presenter.rs
+++ src/ui/presenter.rs
@@ -552,7 +552,7 @@ pub(crate) fn slide_view<'a>(
             .text(slide_text)
             .fill("#fff")
             .shadow(shadow(5, 5, 5, "#000000"))
-            .stroke(stroke(4, "#f00"))
+            // .stroke(stroke(4, "#000"))
             .font(font)
             .view()
             .map(|m| Message::None);
