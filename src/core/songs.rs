9d3fd7f8defed621c00a5d61c0d5b09c14d7e3bc
Chris Cochrun <chris@cochrun.xyz>
2026-02-10 11:49:57 -0600

    some possible benching later

--- src/core/songs.rs
+++ src/core/songs.rs
@@ -1498,6 +1498,23 @@ You saved my soul"
         assert_eq!(name, VerseName::Verse { number: 5 });
     }
 
+    // extern crate test;
+    // use test::{Bencher, black_box};
+
+    // #[bench]
+    // fn bench_pow(b: &mut Bencher) {
+    //     // Optionally include some setup
+    //     let x: f64 = 211.0 * 11.0;
+    //     let y: f64 = 301.0 * 103.0;
+
+    //     b.iter(|| {
+    //         // Inner closure, the actual test
+    //         for i in 1..100 {
+    //             black_box(x.powf(y).powf(x));
+    //         }
+    //     });
+    // }
+
     // #[test]
     // pub fn test_lisp_conversion() {
     //     let value = test_lisp_song();
