3e9882ba75ce9381e6c42df6740bff9b5b578951
Chris Cochrun <chris@cochrun.xyz>
2026-02-18 11:45:49 -0600

    adding a semi working load function with some testing

--- src/core/file.rs
+++ src/core/file.rs
@@ -10,7 +10,7 @@ use std::{
     path::{Path, PathBuf},
 };
 use tar::{Archive, Builder};
-use tracing::error;
+use tracing::{debug, error};
 use zstd::{Decoder, Encoder};
 
 #[allow(clippy::too_many_lines)]
@@ -114,12 +114,14 @@ pub fn save(
         }
         if let Some(path) = audio {
             let file_name = path.file_name().unwrap_or_default();
+            dbg!(&path);
             let mut file = fs::File::open(&path).into_diagnostic()?;
             tar.append_file(file_name, &mut file)
                 .into_diagnostic()?;
         }
         if let Some(background) = background {
             let path = background.path;
+            dbg!(&path);
             let file_name = path.file_name().unwrap_or_default();
             let mut file = fs::File::open(&path).into_diagnostic()?;
             tar.append_file(file_name, &mut file)
@@ -138,6 +140,7 @@ pub fn save(
     fs::remove_dir_all(temp_dir).into_diagnostic()
 }
 
+#[allow(clippy::too_many_lines)]
 pub fn load(path: impl AsRef<Path>) -> Result<Vec<ServiceItem>> {
     let decoder =
         Decoder::new(fs::File::open(&path).into_diagnostic()?)
@@ -162,19 +165,25 @@ pub fn load(path: impl AsRef<Path>) -> Result<Vec<ServiceItem>> {
         .to_os_string()
         .into_string()
         .expect("Should be fine");
-    let save_name = save_name_string.trim_end_matches(save_name_ext);
+    let save_name = save_name_string
+        .trim_end_matches(&format!(".{save_name_ext}"));
     cache_dir.push(save_name);
+
+    if let Err(e) = fs::remove_dir_all(&cache_dir) {
+        debug!("There is no dir here: {e}");
+    }
+    fs::create_dir_all(&cache_dir).into_diagnostic()?;
 
     for entry in tar.entries().into_diagnostic()? {
         let mut entry = entry.into_diagnostic()?;
         entry.unpack_in(&cache_dir).into_diagnostic()?;
     }
 
-    let mut dir = fs::read_dir(cache_dir).into_diagnostic()?;
+    let mut dir = fs::read_dir(&cache_dir).into_diagnostic()?;
     let ron_file = dir
         .find_map(|file| {
             if file.as_ref().ok()?.path().extension()?.to_str()?
-                == ".ron"
+                == "ron"
             {
                 Some(file.ok()?.path())
             } else {
@@ -185,27 +194,219 @@ pub fn load(path: impl AsRef<Path>) -> Result<Vec<ServiceItem>> {
 
     let ron_string =
         fs::read_to_string(ron_file).into_diagnostic()?;
-    let items = ron::de::from_str::<Vec<ServiceItem>>(&ron_string)
-        .into_diagnostic()?;
-    for mut item in items {}
-    todo!()
+    let mut items =
+        ron::de::from_str::<Vec<ServiceItem>>(&ron_string)
+            .into_diagnostic()?;
+    for item in &mut items {
+        let dir = fs::read_dir(&cache_dir).into_diagnostic()?;
+        for file in dir {
+            for slide in &mut item.slides {
+                if let Ok(file) = file.as_ref() {
+                    let file_name = file.file_name();
+                    let audio_path =
+                        slide.audio().clone().unwrap_or_default();
+                    if Some(file_name.as_os_str())
+                        == slide.background.path.file_name()
+                    {
+                        slide.background.path = file.path();
+                    } else if Some(file_name.as_os_str())
+                        == audio_path.file_name()
+                    {
+                        let new_slide = slide
+                            .clone()
+                            .set_audio(Some(file.path()));
+                        *slide = new_slide;
+                    }
+                }
+            }
+            match &mut item.kind {
+                ServiceItemKind::Song(song) => {
+                    if let Ok(file) = file.as_ref() {
+                        let file_name = file.file_name();
+                        let audio_path =
+                            song.audio.clone().unwrap_or_default();
+                        if Some(file_name.as_os_str())
+                            == song
+                                .background
+                                .clone()
+                                .unwrap_or_default()
+                                .path
+                                .file_name()
+                        {
+                            let background = song.background.clone();
+                            song.background =
+                                background.map(|mut background| {
+                                    background.path = file.path();
+                                    background
+                                });
+                        } else if Some(file_name.as_os_str())
+                            == audio_path.file_name()
+                        {
+                            song.audio = Some(file.path());
+                        }
+                    }
+                }
+                ServiceItemKind::Video(video) => {
+                    if let Ok(file) = file.as_ref() {
+                        let file_name = file.file_name();
+                        if Some(file_name.as_os_str())
+                            == video.path.file_name()
+                        {
+                            video.path = file.path();
+                        }
+                    }
+                }
+                ServiceItemKind::Image(image) => {
+                    if let Ok(file) = file.as_ref() {
+                        let file_name = file.file_name();
+                        if Some(file_name.as_os_str())
+                            == image.path.file_name()
+                        {
+                            image.path = file.path();
+                        }
+                    }
+                }
+                ServiceItemKind::Presentation(presentation) => {
+                    if let Ok(file) = file.as_ref() {
+                        let file_name = file.file_name();
+                        if Some(file_name.as_os_str())
+                            == presentation.path.file_name()
+                        {
+                            presentation.path = file.path();
+                        }
+                    }
+                }
+                ServiceItemKind::Content(_slide) => todo!(),
+            }
+        }
+    }
+    Ok(items)
 }
 
 #[cfg(test)]
 mod test {
+    use pretty_assertions::assert_eq;
+
     use super::*;
-    use crate::core::{slide::Slide, songs::Song};
-    use std::path::PathBuf;
+    use crate::core::{
+        service_items::ServiceTrait,
+        slide::{Slide, TextAlignment},
+        songs::{Song, VerseName},
+    };
+    use std::{collections::HashMap, path::PathBuf};
+
+    fn test_song() -> Song {
+        let lyrics = "Some({Verse(number:4):\"Our Savior displayed\\nOn a criminal\\'s cross\\n\\nDarkness rejoiced as though\\nHeaven had lost\\n\\nBut then Jesus arose\\nWith our freedom in hand\\n\\nThat\\'s when death was arrested\\nAnd my life began\\n\\nThat\\'s when death was arrested\\nAnd my life began\",Intro(number:1):\"Death Was Arrested\\nNorth Point Worship\",Verse(number:3):\"Released from my chains,\\nI\\'m a prisoner no more\\n\\nMy shame was a ransom\\nHe faithfully bore\\n\\nHe cancelled my debt and\\nHe called me His friend\\n\\nWhen death was arrested\\nAnd my life began\",Bridge(number:1):\"Oh, we\\'re free, free,\\nForever we\\'re free\\n\\nCome join the song\\nOf all the redeemed\\n\\nYes, we\\'re free, free,\\nForever amen\\n\\nWhen death was arrested\\nAnd my life began\\n\\nOh, we\\'re free, free,\\nForever we\\'re free\\n\\nCome join the song\\nOf all the redeemed\\n\\nYes, we\\'re free, free,\\nForever amen\\n\\nWhen death was arrested\\nAnd my life began\",Other(number:99):\"When death was arrested\\nAnd my life began\\n\\nThat\\'s when death was arrested\\nAnd my life began\",Verse(number:2):\"Ash was redeemed\\nOnly beauty remains\\n\\nMy orphan heart\\nWas given a name\\n\\nMy mourning grew quiet,\\nMy feet rose to dance\\n\\nWhen death was arrested\\nAnd my life began\",Verse(number:1):\"Alone in my sorrow\\nAnd dead in my sin\\n\\nLost without hope\\nWith no place to begin\\n\\nYour love made a way\\nTo let mercy come in\\n\\nWhen death was arrested\\nAnd my life began\",Chorus(number:1):\"Oh, Your grace so free,\\nWashes over me\\n\\nYou have made me new,\\nNow life begins with You\\n\\nIt\\'s Your endless love,\\nPouring down on us\\n\\nYou have made us new,\\nNow life begins with You\"})".to_string();
+        let verse_map: Option<HashMap<VerseName, String>> =
+            ron::from_str(&lyrics).unwrap();
+        Song {
+            id: 7,
+            title: "Death Was Arrested".to_string(),
+            lyrics: Some(lyrics),
+            author: Some(
+                "North Point Worship".to_string(),
+            ),
+            ccli: None,
+            audio: Some("/home/chris/music/North Point InsideOut/Nothing Ordinary, Pt. 1 (Live)/05 Death Was Arrested (feat. Seth Condrey).mp3".into()),
+            verse_order: Some(vec!["Some([Chorus(number:1),Intro(number:1),Other(number:99),Bridge(number:1),Verse(number:4),Verse(number:2),Verse(number:3),Verse(number:1)])".to_string()]),
+            background: Some(Background::try_from("/home/chris/nc/tfc/openlp/Flood/motions/Ocean_Floor_HD.mp4").unwrap()),
+            text_alignment: Some(TextAlignment::MiddleCenter),
+            font: Some("Quicksand Bold".to_string()),
+            font_size: Some(80),
+            stroke_size: None,
+            verses: Some(vec![VerseName::Chorus { number: 1 }, VerseName::Intro { number: 1 }, VerseName::Other { number: 99 }, VerseName::Bridge { number: 1 }, VerseName::Verse { number: 4 }, VerseName::Verse { number: 2 }, VerseName::Verse { number: 3 }, VerseName::Verse { number: 1 }
+            ]),
+            verse_map,
+            ..Default::default()
+        }
+    }
 
     fn get_items() -> Vec<ServiceItem> {
+        let song = test_song();
+        let slides = song.to_slides().unwrap();
         let items = vec![ServiceItem {
             database_id: 7,
-            kind: ServiceItemKind::Song(Song::default()),
+            kind: ServiceItemKind::Song(song),
             id: 0,
             title: "Glory Glory".into(),
-            slides: vec![Slide::default()],
+            slides: slides,
         }];
         items
+    }
+
+    #[test]
+    fn test_load() {
+        let path = PathBuf::from("./test.pres");
+        let result = load(&path);
+        match result {
+            Ok(items) => {
+                assert!(items.len() > 0);
+                // assert_eq!(items, get_items());
+                let cache_dir = cache_dir();
+                assert!(fs::read_dir(&cache_dir).is_ok());
+                assert!(
+                    find_paths(items),
+                    "Some paths must not have the cache_dir in it's path"
+                )
+            }
+            Err(e) => assert!(false, "{e}"),
+        }
+    }
+
+    // checks to make sure all paths in slides and items point to cache_dir
+    fn find_paths(items: Vec<ServiceItem>) -> bool {
+        let cache_dir = cache_dir();
+        items.iter().all(|item| {
+            match &item.kind {
+                ServiceItemKind::Song(song) => {
+                    if let Some(bg) = &song.background {
+                        if !bg.path.starts_with(&cache_dir) {
+                            return false;
+                        }
+                    }
+                    if let Some(audio) = &song.audio {
+                        if !audio.starts_with(&cache_dir) {
+                            return false;
+                        }
+                    }
+                }
+                ServiceItemKind::Video(video) => {
+                    if !video.path.starts_with(&cache_dir) {
+                        return false;
+                    }
+                }
+                ServiceItemKind::Image(image) => {
+                    if !image.path.starts_with(&cache_dir) {
+                        return false;
+                    }
+                }
+                ServiceItemKind::Presentation(presentation) => {
+                    if !presentation.path.starts_with(&cache_dir) {
+                        return false;
+                    }
+                }
+                ServiceItemKind::Content(slide) => todo!(),
+            }
+            for slide in &item.slides {
+                if !slide.background().path.starts_with(&cache_dir) {
+                    return false;
+                }
+                if !slide.audio().map_or(true, |audio| {
+                    audio.starts_with(&cache_dir)
+                }) {
+                    return false;
+                }
+            }
+            true
+        })
+    }
+
+    fn cache_dir() -> PathBuf {
+        let mut cache_dir = dirs::cache_dir().unwrap();
+        cache_dir.push("lumina");
+        cache_dir.push("cached_save_files");
+        cache_dir.push("test");
+        cache_dir
     }
 
     #[test]
